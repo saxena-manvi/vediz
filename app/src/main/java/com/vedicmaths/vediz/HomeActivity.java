0f035b6bf3a187f7b5e78e217caa2a29d7adbdd1
DESKTOP-7S7GQNM\DELL <saxena.manvi369@gmail.com>
2021-03-01 20:34:16 +0530

    initial commit for vediz

---
+++ app/src/main/java/com/vedicmaths/vediz/HomeActivity.java
@@ -0,0 +1,128 @@
+package com.vedicmaths.vediz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.muddzdev.styleabletoastlibrary.StyleableToast;
+
+import org.jetbrains.annotations.NotNull;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    public HomeActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+   @Override
+    public boolean onOptionsItemSelected(@NotNull MenuItem item) {
+
+        int id = item.getItemId();
+
+       if(id == R.id.action_privacy){
+
+            Intent intent = new Intent(HomeActivity.this,PolicyActivity.class);
+            startActivity(intent);
+           finish();
+
+          return true;
+     }
+      else
+      if (id == R.id.action_about){
+
+          StyleableToast.makeText(getApplicationContext(),"Learning Vedic Maths!",R.style.rightToast).show();
+
+
+           return true;
+
+      }
+
+     else
+    if (id == R.id.action_contact){
+
+            StyleableToast.makeText(getApplicationContext(),"vedizmaths@gmail.com",R.style.rightToast).show();
+
+
+           return true;
+       }
+
+
+       else
+       if (id == R.id.action_exit){
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+           builder.setMessage("Do you want to exit?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", (dialogInterface, i) -> HomeActivity.super.onBackPressed())
+                    .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel());
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+
+            return true;
+
+        }
+        else
+
+
+            return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
